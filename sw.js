c5aacc11e09753919603bd3db2533746f6504f26
Booligoosh <booligoosh@optusnet.com.au>
2017-10-17 16:20:27 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,5 +3,5 @@ importScripts('cache-polyfill.js');
 self.addEventListener('fetch', function(event) {
   console.log(event.request.hostname);
   console.log(fetch('https://cors-anywhere.herokuapp.com/' + event.request));
-  event.respondWith(fetch('https://cors-anywhere.herokuapp.com/' + event.request));
+  event.respondWith(event.respondWith(new Response(fetch('https://cors-anywhere.herokuapp.com/' + event.request)));
 });
